7078e2dded760ac985752eaf15fb0641421afd1f
Gerson <44945972+ItsGers8@users.noreply.github.com>
2020-09-03 11:52:38 +0200

    Wachtwoord verbetering toegevoegd

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -5,12 +8,14 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/ovchip?user=postgres&password=PASSWORDHERE";
         try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Wachtwoord: ");
+            String url = "jdbc:postgresql://localhost:5432/ovchip?user=postgres&password=" + br.readLine();
             ReizigerDAO reizigerDAO = new ReizigerDAOPsql(DriverManager.getConnection(url));
             reizigerDAO.delete(reizigerDAO.findById(77));
             testReizigerDAO(reizigerDAO);
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
     }
