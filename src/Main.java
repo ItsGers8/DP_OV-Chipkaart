5028dfe20a8a6e239d960648f7ac725b8f0fc612
Gerson <44945972+ItsGers8@users.noreply.github.com>
2020-09-03 11:40:07 +0200

    Finished exercise 1

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,9 @@ public class Main {
     public static void main(String[] args) {
         String url = "jdbc:postgresql://localhost:5432/ovchip?user=postgres&password=PASSWORDHERE";
         try {
-            ReizigerDAOPsql reizigerDAOPsql = new ReizigerDAOPsql(DriverManager.getConnection(url));
-//            testReizigerDAO(new ReizigerDAOPsql(DriverManager.getConnection(url)));
-//            System.out.println(reizigerDAOPsql.save(new Reiziger(6, "G", null, "Mak", LocalDate.of(2002, 12, 3))));
-//            System.out.println(reizigerDAOPsql.delete(reizigerDAOPsql.findById(6)));
-//            System.out.println(reizigerDAOPsql.findById(5) == null ? "Niet gevonden" : reizigerDAOPsql.findById(5));
-            for (Reiziger reiziger : reizigerDAOPsql.findByGbdatum("2002-12-03")) {
-                System.out.println(reiziger.toString());
-            }
+            ReizigerDAO reizigerDAO = new ReizigerDAOPsql(DriverManager.getConnection(url));
+            reizigerDAO.delete(reizigerDAO.findById(77));
+            testReizigerDAO(reizigerDAO);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -31,7 +26,6 @@ public class Main {
         System.out.println();
 
         // Maak een nieuwe reiziger aan en persisteer deze in de database
-        String gbdatum = "1981-03-14";
         Reiziger sietske = new Reiziger(77, "S", "", "Boers", LocalDate.of(1981, 3, 14));
         System.out.print("[Test] Eerst " + reizigers.size() + " reizigers, na ReizigerDAO.save() ");
         rdao.save(sietske);
